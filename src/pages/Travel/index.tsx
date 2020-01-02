84f7b5b3040285f3531c76d53dbcb0f062207b30
Andrew Ho <ho@thundermac.local>
2020-01-02 00:03:45 -0800

    updating Travel page

--- src/pages/Travel/index.tsx
+++ src/pages/Travel/index.tsx
@@ -2,8 +2,66 @@ import React from 'react';
 
 import styles from './Travel.module.css';
 
+import splashImg from 'assets/main2.jpg';
+
 const Travel: React.FC = () => {
-	return <div className={styles.red}>This is the travel page</div>;
+	return (
+		<div className={styles.content}>
+			<section className="splash img-container">
+				<img
+					src={splashImg}
+					alt="splash"
+					className="img-container__img--vertical-center img-container__img--horizontal-center"
+				/>
+			</section>
+
+			<section className={styles.content}>
+				<h1>By Plane</h1>
+
+				<section>
+					<h2>Shannon Airport</h2>
+					<p>
+						Closest airport to the hotel venue, only an hour drive away. But
+						flights might be more expensive..
+					</p>
+					<p>
+						<a href="#">Map Route</a>
+					</p>
+				</section>
+
+				<section>
+					<h2>Dublin Airport</h2>
+					<p>
+						Fly into the capital city of Ireland and drive for about 3 hours.
+						Travelers from Asia will most likely be flying into this airport.
+					</p>
+					<p>
+						<a href="#">Map Route</a>
+					</p>
+				</section>
+			</section>
+
+			<hr />
+
+			<section className={styles.content}>
+				<h1>By Plane</h1>
+
+				<p>
+					We highly recommend renting a car to drive through the countryside.
+					Most cars in Ireland are manual, if you need an automatic one, book as
+					early as possible before they run out.
+				</p>
+
+				<p>
+					If you do not drive, please email us at{' '}
+					<a href="mailto:maryandalanwedding@gmail.com" target="_blank">
+						maryandalanwedding@gmail.com
+					</a>{' '}
+					so we can coordinate transportation.
+				</p>
+			</section>
+		</div>
+	);
 };
 
 export default Travel;
